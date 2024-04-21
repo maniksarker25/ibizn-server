5131ae14e089408241e924525fc9b8b0dc1eda19
Manik <tonysins43@gmail.com>
2024-04-21 10:43:38 +0600

    some changes in package and intenary modal

--- src/models/package.model.js
+++ src/models/package.model.js
@@ -1,52 +1,56 @@
 const { Schema, model } = require("mongoose");
 
-const packageSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const packageSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    packageName: {
+      type: String,
+      required: true,
+    },
+    numberOfDay: {
+      type: Number,
+      required: true,
+    },
+    roomType: {
+      type: String,
+      required: true,
+    },
+    numberOfDivies: {
+      type: Number,
+      required: true,
+    },
+    fullBoard: {
+      type: Boolean,
+      default: false,
+    },
+    breakfast: {
+      type: Boolean,
+      default: false,
+    },
+    upgradeable: {
+      type: Boolean,
+      default: false,
+    },
+    divingCourses: {
+      type: Boolean,
+      default: false,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    resortDailySchedule: {
+      type: String,
+      required: true,
+    },
   },
-  packageName: {
-    type: String,
-    required: true,
-  },
-  numberOfDay: {
-    type: Number,
-    required: true,
-  },
-  roomType: {
-    type: String,
-    required: true,
-  },
-  numberOfDivies: {
-    type: Number,
-    required: true,
-  },
-  fullBoard: {
-    type: Boolean,
-    default: false,
-  },
-  breakfast: {
-    type: Boolean,
-    default: false,
-  },
-  upgradeable: {
-    type: Boolean,
-    default: false,
-  },
-  divingCourses: {
-    type: Boolean,
-    default: false,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-},
-{
-  timestamps: true,
-}
-
+  {
+    timestamps: true,
+  }
 );
 
 const Package = model("Package", packageSchema);
