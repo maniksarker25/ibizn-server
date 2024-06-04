1135e09d384eebc069d1ccdbae81e0297ea574db
Manik <tonysins43@gmail.com>
2024-06-05 03:28:18 +0600

    s

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(express.json({ limit: "10mb" }));
 app.use(cors({ origin: "https://next-ibizn.vercel.app" }));
 // app.use(cors({ origin: "http://localhost:3000" }));
-
+// make some changes
 // import all routes ---------
 
 const globalErrorHandler = require("./utilities/globalErrorHandler");
